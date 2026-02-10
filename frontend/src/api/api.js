424c32d1f197aeaafd3ead0a4500d1b040146499
leoborges12 <leonardoab.aluno@unipampa.edu.br>
2026-02-10 12:24:44 -0300

    Configura baseURL via VITE_API_URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL,
 });
